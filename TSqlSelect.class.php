872c54fc3f2c12f1fb7455c5a0e05128bbb23fcb
Gabriela <gabrielamoraesribeiro.moraes@gmail.com>
2024-10-07 14:44:57 -0300

    Update TSqlSelect.class.php

--- TSqlSelect.class.php
+++ TSqlSelect.class.php
@@ -1,61 +1,64 @@
 <?php
+/* classe TSqlSelect
+* Esta classe prove meios para manipulação de uma instrução SQL de Select no BD
+*/
 
-// classe TSqlSelect
-// Esta Classe provê meios para manipulação de uma instrução SQL de select no BD
+final class TSqlSelect extends TSqlInstruction {
 
-final class TSqlSelect extends TSqlInstruction{
+    private $columns; // array de colunas a serem retornadas.
 
-    private $column; // array de colunas a serem retornadas
-
-    // Método addColumn 
-    // adiciona uma coluna a ser retornada pelo SELECT
-    // @param $column = coluna da tabela 
+    /*Método addColumn
+    * adiciona uma coluna a ser retornada pelo SELECT
+    * @param $column = coluna da tabela
+    */
 
     public function addColumn($column){
-        $this->columns[] = $column;    
+        $this->columns[] = $column;
     }
+    /* método getInstruction()
+    * retorna a instrução de SELECT em forma de string
+    */
 
-    // Método getInstruction
-    // retorna a instrução de SELECT em forma de string
-
-    public function getInstruction(){
+    public function getInstruction()
+    {
         // monta a instrução SQL
+
         $this->sql = 'SELECT';
 
-        // monta string com os nomes das colunas
-        $this->sql .= implode(',', $this->columns);
+        //monta string com os nomes das colunas 
+        $this->sql .= implode(',',$this->columns);
 
-        // adiciona na clausula FROM o nome da tabela
-        $this->sql .= 'FROM' . $this->entity;
+        //adiciona na clausula FROM o nome da tabela
+        $this->sql .='FROM' . $this->entity;
 
-        // obtem a clausula where do objeto criteria
+        //obtem a clausula where do objeto criteria
         if($this->criteria){
             $expression = $this->criteria->dump();
             if($expression){
-                $this->sql .= 'WHERE' . $expression;
+                $this->sql .='WHERE' . $expression;
+            }
+
+            //obtem as propriedades do critério
+
+            $order = $this->criteria->getProperty('order');
+            $limit = $this->criteria->getProperty('limit');
+            $offset = $this->criteria->getProperty('offset');
+
+            // obtem a ordenação do SELECT
+
+            if($order){
+                $this->sql.= 'ORDER BY'.$order;
+            }
+            if($limit){
+                $this->sql .= 'LIMIT'.$limit;
+            }
+            if($offset){    
+                $this->sql .= 'OFFSET'.$offset;
         }
-
-        // obtem as propriedades do criteria 
-
-        $order = $this->criteria->getProperty('order');
-        $limit = $this->criteria->getProperty('limit');
-        $offset = $this->criteria->getProperty('offset');
-
-        // obtem a ordenação do SELECT
-
-        if($order){
-            $this->sql .=;
-        }
-
-
     }
-    
+    return $this->sql;
 }
 
-
-
-
-
-
+}
 
 ?>
